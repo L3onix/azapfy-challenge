3ab6a296edd05e45950b355ebd9a78fd0aaa812e
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-01-02 00:27:03 -0300

    criando gate para regra de autorização de acesso a NFE

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use App\Models\Nfe;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +24,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('isNfeOwner', function (User $user, Nfe $nfe) {
+            return $user->id === $nfe->user_id ?  Response::allow() : Response::deny('You must be a NFE owner');
+        });
     }
 }
