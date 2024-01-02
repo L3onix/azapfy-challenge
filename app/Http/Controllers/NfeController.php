89c68870704e29fd50bb264eb7babd8b2d086656
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-01-02 00:25:15 -0300

    adicionando metodos CRUD para NFE

--- app/Http/Controllers/NfeController.php
+++ app/Http/Controllers/NfeController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Nfe;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class NfeController extends Controller
 {
-    //
+    public function show(Request $request) {
+        return true;
+        // $nfe = Nfe::findById(1);
+        // $response = Gate::inspect('isNfeOwner', $nfe);
+
+        // if ($response->denied()) {
+        //     return $response->message();
+        // }
+        // return true;
+    }
+
+    public function store(Request $request) {
+        return true;
+    }
+
+    public function update(Request $request) {
+        return true;
+    }
+
+    public function destroy(Request $request) {
+        return true;
+    }
+
 }
