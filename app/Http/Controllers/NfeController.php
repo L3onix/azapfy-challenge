242b780372db6f772e2d4fe97ab96211f4d8792a
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-01-02 23:34:36 -0300

    Implementação de métodos de rota

--- app/Http/Controllers/NfeController.php
+++ app/Http/Controllers/NfeController.php
@@ -2,33 +2,146 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreNfeRequest;
 use App\Models\Nfe;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Validator;
 
 class NfeController extends Controller
 {
+    /**
+     * Show NFE filter by id
+     * @param Request $request
+     * @return Response
+     */
     public function show(Request $request) {
-        return true;
-        // $nfe = Nfe::findById(1);
-        // $response = Gate::inspect('isNfeOwner', $nfe);
+        try {
+            $nfe = Nfe::find($request->route('nfe'));
+            $response = Gate::inspect('isNfeOwner', $nfe);
 
-        // if ($response->denied()) {
-        //     return $response->message();
-        // }
-        // return true;
+            if ($response->denied()) {
+                return response()->json([
+                    'message' => 'Permission error',
+                    'error' => $response->message()
+                ]);
+            }
+            return response()->json([
+                'message' => '',
+                'nfe' => $nfe
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Exception error',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
-    public function store(Request $request) {
-        return true;
+    /**
+     * Index a list of NFE filter by user id
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request) {
+        try {
+            $nfes = Nfe::where('user_id', auth()->user()->id)->get();
+
+            return response()->json([
+                'message' => '',
+                'nfe' => $nfes
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Exception error',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
-    public function update(Request $request) {
-        return true;
+    /**
+     * Create a new NFE
+     * @param StoreNfeRequest $request
+     * @return Response
+     */
+    public function store(StoreNfeRequest $request) {
+        try {
+            $validated = $request->validated();
+            $validated['user_id'] = auth()->user()->id;
+
+            $nfe = Nfe::create($validated);
+
+            return response()->json([
+                'message' => 'NFE created',
+                'nfe' => $nfe
+            ], 201);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Exception error',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
+    /**
+     * Update a NFE by id
+     * @param StoreNfeRequest $request
+     * @return Response
+     */
+    public function update(StoreNfeRequest $request) {
+        try {
+            $validated = $request->validated();
+            $validated['user_id'] = auth()->user()->id;
+
+            $nfe = Nfe::findOrFail($request->route('nfe'));
+            $response = Gate::inspect('isNfeOwner', $nfe);
+            if ($response->denied()) {
+                return response()->json([
+                    'message' => 'Permission error',
+                    'error' => $response->message()
+                ]);
+            }
+            $nfe->update($validated);
+
+            return response()->json([
+                'message' => 'NFE updated',
+                'nfe' => $nfe
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Exception error',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    /**
+     * Delete a NFE by id
+     * @param Request $request
+     * @return Response
+     */
     public function destroy(Request $request) {
-        return true;
+        try {
+            $nfe = Nfe::findOrFail($request->route('nfe'));
+            $response = Gate::inspect('isNfeOwner', $nfe);
+            if ($response->denied()) {
+                return response()->json([
+                    'message' => 'Permission error',
+                    'error' => $response->message()
+                ]);
+            }
+            $nfe->delete();
+
+            return response()->json([
+                'message' => 'NFE destroyed',
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Exception error',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
 }
